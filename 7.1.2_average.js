3e974679c87d43532f5617e58f69f6fc97484736
Rocky Haque <rockyhaque99@gmail.com>
2024-01-13 15:29:29 +0600

    Update 7.1.2_average.js

--- 7.1.2_average.js
+++ 7.1.2_average.js
@@ -1,3 +1,31 @@
+/*
+#Three Number Average
+
+Write a program that finds the average of three numbers entered by the user.
+
+Input
+The input consists of 3 floats.
+
+Output
+Output will be a float number.
+
+Constraints
+1.2E-38 ≤ |S| ≤ 3.4E+38
+
+The program should print the number with exactly two decimal points.
+
+Example:
+Enter three numbers
+
+Input:
+10 20 30
+
+Output:
+Average: 20.00
+
+*/
+
+
 function main (input) {
     const data = input.split(" ");
     let sum = 0;
