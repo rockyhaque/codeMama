d4c1211302f0e3f65d970690dd6ce4c0c1908246
Rocky Haque <rockyhaque99@gmail.com>
2024-01-13 15:37:43 +0600

    Create 7.1.3_maxNum.js

---
+++ 7.1.3_maxNum.js
@@ -0,0 +1,40 @@
+/*
+7.1.3 Greater in Three
+
+Write a program that finds the maximum of three numbers entered by the user.
+
+Input
+The input consists of 3 integers.
+
+Output
+Output the maximum
+
+Constraints
+-2 31 ≤ |S| ≤ 231 - 1
+
+Example:
+Enter three numbers
+
+Input:
+10 20 30
+
+Output:
+30
+
+*/
+
+
+function main (input) {
+    const data = input.split(" ");
+    const sorted = data.sort();
+    console.log(sorted.pop());
+}
+
+let input = "";
+process.stdin.on('data', (chunk) => {
+    input += chunk;
+});
+
+process.stdin.on('end', () => {
+    main(input.trim());
+});
