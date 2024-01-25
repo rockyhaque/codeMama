dae2f51ea5cc53a686c747205e4e87844bee51aa
Rocky Haque <rockyhaque99@gmail.com>
2024-01-25 12:11:15 +0600

    Create 8.2.6_minimal_yet.js

---
+++ 8.2.6_minimal_yet.js
@@ -0,0 +1,24 @@
+/*
+Minimal yet Important
+
+Problem Statement
+Write a program that finds the minimum of three numbers entered by the user.
+
+Input
+The input consists of 3 integers.
+
+Output
+Output the minimum.
+
+Constraints
+-2 31 ≤ |S| ≤ 231 - 1
+Example:
+Enter three numbers
+
+Input:
+10 20 30
+Output:
+10
+
+*/
+
